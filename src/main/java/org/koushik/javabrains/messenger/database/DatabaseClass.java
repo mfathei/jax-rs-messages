efad4119153654e655be0c1bbae0290a03290bd4
Mohamed Mahdy <oracle.dev10g@gmail.com>
2018-03-15 10:58:02 +0200

    add Service stubs

---
+++ src/main/java/org/koushik/javabrains/messenger/database/DatabaseClass.java
@@ -0,0 +1,16 @@
+package org.koushik.javabrains.messenger.database;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.koushik.javabrains.messenger.model.Message;
+
+public class DatabaseClass {
+
+	private static Map<Long, Message> messages = new HashMap<>();
+
+	public static Map<Long, Message> getMessages() {
+		return messages;
+	}
+
+}
--- src/main/java/org/koushik/javabrains/messenger/service/MessageService.java
+++ src/main/java/org/koushik/javabrains/messenger/service/MessageService.java
@@ -2,18 +2,43 @@ package org.koushik.javabrains.messenger.service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
+import org.koushik.javabrains.messenger.database.DatabaseClass;
 import org.koushik.javabrains.messenger.model.Message;
 
 public class MessageService {
 
-	public List<Message> getAllMessages() {
-		Message m1 = new Message(1, "Hello World", "koushik");
-		Message m2 = new Message(2, "Hello Jersey", "koushik");
-		List<Message> ms = new ArrayList<>();
-		ms.add(m1);
-		ms.add(m2);
-		return ms;
+	private Map<Long, Message> messages = DatabaseClass.getMessages();
+
+	public MessageService() {
+		messages.put(1L, new Message(1, "Hello World", "koushik"));
+		messages.put(2L, new Message(2, "Hello Jersey", "koushik"));
 	}
 
+	public List<Message> getAllMessages() {
+		return new ArrayList<Message>(messages.values());
+	}
+
+	public Message getMessage(Long id) {
+		return messages.get(id);
+	}
+
+	public Message addMessage(Message msg) {
+		msg.setId(messages.size() + 1);
+		messages.put(msg.getId(), msg);
+		return msg;
+	}
+
+	public Message updateMessage(Message msg) {
+		if (msg.getId() <= 0)
+			return null;
+
+		messages.put(msg.getId(), msg);
+		return msg;
+	}
+
+	public Message deleteMessage(Long id) {
+		return messages.remove(id);
+	}
 }
