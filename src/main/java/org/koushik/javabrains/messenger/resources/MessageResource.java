c58aaa788dd0fd898ba4a39b3764f6310e03a953
Mohamed Mahdy <oracle.dev10g@gmail.com>
2018-03-15 13:31:12 +0200

    optimize annotations

--- src/main/java/org/koushik/javabrains/messenger/resources/MessageResource.java
+++ src/main/java/org/koushik/javabrains/messenger/resources/MessageResource.java
@@ -3,8 +3,10 @@ package org.koushik.javabrains.messenger.resources;
 import java.util.List;
 
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -14,28 +16,38 @@ import org.koushik.javabrains.messenger.model.Message;
 import org.koushik.javabrains.messenger.service.MessageService;
 
 @Path("/messages")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
 public class MessageResource {
 
-	MessageService service = new MessageService();
+	MessageService messageService = new MessageService();
 
 	@GET
-	@Produces(MediaType.APPLICATION_JSON)
 	public List<Message> getMessages() {
-		return service.getAllMessages();
+		return messageService.getAllMessages();
 	}
 	
 	@GET
 	@Path("/{msgId}")
-	@Produces(MediaType.APPLICATION_JSON)
 	public Message getMessage(@PathParam("msgId") long id) {
-		return service.getMessage(id);
+		return messageService.getMessage(id);
 	}
 	
 	@POST
-	@Consumes(MediaType.APPLICATION_JSON)
-	@Produces(MediaType.APPLICATION_JSON)
 	public Message addMessage(Message msg) {
-		return service.addMessage(msg);
+		return messageService.addMessage(msg);
 	}
-
+	
+	@PUT
+	@Path("/{msgId}")
+	public Message addMessage(@PathParam("msgId") long id, Message msg) {
+		msg.setId(id);
+		return messageService.updateMessage(msg);
+	}
+	
+	@DELETE
+	@Path("/{msgId}")
+	public void deleteMessage(@PathParam("msgId") long id) {
+		messageService.deleteMessage(id);
+	}
 }
