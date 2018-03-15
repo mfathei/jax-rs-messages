16ee03f591b0ee01037a3ee0ba79efc1a33500bc
Mohamed Mahdy <oracle.dev10g@gmail.com>
2018-03-15 12:53:13 +0200

    implementing POST method

--- src/main/java/org/koushik/javabrains/messenger/resources/MessageResource.java
+++ src/main/java/org/koushik/javabrains/messenger/resources/MessageResource.java
@@ -2,7 +2,9 @@ package org.koushik.javabrains.messenger.resources;
 
 import java.util.List;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -27,6 +29,13 @@ public class MessageResource {
 	@Produces(MediaType.APPLICATION_JSON)
 	public Message getMessage(@PathParam("msgId") long id) {
 		return service.getMessage(id);
+	}
+	
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Message addMessage(Message msg) {
+		return service.addMessage(msg);
 	}
 
 }
