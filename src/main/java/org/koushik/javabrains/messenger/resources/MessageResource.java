26f6c7def8fb130019fe3f14b55e9769d0ab67af
Mohamed Mahdy <oracle.dev10g@gmail.com>
2018-03-16 21:38:54 +0200

    Sending status codes and location headers

--- src/main/java/org/koushik/javabrains/messenger/resources/MessageResource.java
+++ src/main/java/org/koushik/javabrains/messenger/resources/MessageResource.java
@@ -1,5 +1,6 @@
 package org.koushik.javabrains.messenger.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import javax.ws.rs.BeanParam;
@@ -11,7 +12,10 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
 
 import org.koushik.javabrains.messenger.model.Message;
 import org.koushik.javabrains.messenger.resources.beans.MessageFilterBean;
@@ -22,47 +26,53 @@ import org.koushik.javabrains.messenger.service.MessageService;
 @Produces(MediaType.APPLICATION_JSON)
 public class MessageResource {
 
-	MessageService messageService = new MessageService();
+    MessageService messageService = new MessageService();
 
-	@GET
-	public List<Message> getMessages(@BeanParam MessageFilterBean filterBean) {
-		if (filterBean.getYear() > 0) {
-			return messageService.getAllMessagesForYear(filterBean.getYear());
-		}
+    @GET
+    public List<Message> getMessages(@BeanParam MessageFilterBean filterBean) {
+        if (filterBean.getYear() > 0) {
+            return messageService.getAllMessagesForYear(filterBean.getYear());
+        }
 
-		if (filterBean.getStart() >= 0 && filterBean.getSize() > 0) {
-			return messageService.getAllMessagesPaginated(filterBean.getStart(), filterBean.getSize());
-		}
+        if (filterBean.getStart() >= 0 && filterBean.getSize() > 0) {
+            return messageService.getAllMessagesPaginated(filterBean.getStart(), filterBean.getSize());
+        }
 
-		return messageService.getAllMessages();
-	}
+        return messageService.getAllMessages();
+    }
 
-	@GET
-	@Path("/{msgId}")
-	public Message getMessage(@PathParam("msgId") long id) {
-		return messageService.getMessage(id);
-	}
+    @GET
+    @Path("/{msgId}")
+    public Message getMessage(@PathParam("msgId") long id) {
+        return messageService.getMessage(id);
+    }
 
-	@POST
-	public Message addMessage(Message msg) {
-		return messageService.addMessage(msg);
-	}
+    @POST
+    public Response addMessage(@Context UriInfo uriInfo, Message msg) {
+        Message message = messageService.addMessage(msg);
+        String id = String.valueOf(message.getId());
+        URI path = uriInfo.getAbsolutePathBuilder().path(id).build();
+//        return Response.status(Response.Status.CREATED)
+        return Response.created(path)
+                .entity(message)
+                .build();
+    }
 
-	@PUT
-	@Path("/{msgId}")
-	public Message addMessage(@PathParam("msgId") long id, Message msg) {
-		msg.setId(id);
-		return messageService.updateMessage(msg);
-	}
+    @PUT
+    @Path("/{msgId}")
+    public Message addMessage(@PathParam("msgId") long id, Message msg) {
+        msg.setId(id);
+        return messageService.updateMessage(msg);
+    }
 
-	@DELETE
-	@Path("/{msgId}")
-	public void deleteMessage(@PathParam("msgId") long id) {
-		messageService.deleteMessage(id);
-	}
+    @DELETE
+    @Path("/{msgId}")
+    public void deleteMessage(@PathParam("msgId") long id) {
+        messageService.deleteMessage(id);
+    }
 
-	@Path("/{msgId}/comments")
-	public CommentResource getComments() {
-		return new CommentResource();
-	}
+    @Path("/{msgId}/comments")
+    public CommentResource getComments() {
+        return new CommentResource();
+    }
 }
