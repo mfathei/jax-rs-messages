b8bc9e8a53ab2f193c6e3699fcce60f34e5bb110
Mohamed Mahdy <oracle.dev10g@gmail.com>
2018-03-16 21:21:55 +0200

    implementing sub resources

--- src/main/java/org/koushik/javabrains/messenger/resources/CommentResource.java
+++ src/main/java/org/koushik/javabrains/messenger/resources/CommentResource.java
@@ -3,8 +3,10 @@ package org.koushik.javabrains.messenger.resources;
 import java.util.List;
 
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -19,24 +21,32 @@ import org.koushik.javabrains.messenger.service.CommentService;
 
 public class CommentResource {
 
-	private CommentService commentService = new CommentService();
+    private CommentService commentService = new CommentService();
 
-	@GET
-	public List<Comment> getAllComments(@PathParam("msgId") long messageId) {
-		return commentService.getAllComments(messageId);
-	}
-	
-	@POST
-	public Comment addComment(@PathParam("msgId") long messageId, Comment comment) {
-		return commentService.addComment(messageId, comment);
-	}
+    @GET
+    public List<Comment> getAllComments(@PathParam("msgId") long messageId) {
+        return commentService.getAllComments(messageId);
+    }
 
-	@GET
-	@Path("/{commentId}")
-	public Comment getComment(@PathParam("msgId") long messageId, @PathParam("commentId") long commentId) {
-		return commentService.getComment(messageId, commentId);
-	}
+    @GET
+    @Path("/{commentId}")
+    public Comment getComment(@PathParam("msgId") long messageId, @PathParam("commentId") long commentId) {
+        return commentService.getComment(messageId, commentId);
+    }
 
+    @POST
+    public Comment addComment(@PathParam("msgId") long messageId, Comment comment) {
+        return commentService.addComment(messageId, comment);
+    }
 
+    @PUT
+    public Comment updateComment(@PathParam("msgId") long messageId, Comment comment) {
+        return commentService.updateComment(messageId, comment);
+    }
 
+    @DELETE
+    @Path("/{commentId}")
+    public Comment deleteComment(@PathParam("msgId") long messageId, @PathParam("commentId") long commentId) {
+        return commentService.deleteComment(messageId, commentId);
+    }
 }
